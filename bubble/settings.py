0d3f4537a6dfd5dc5ac2d303e27fb6885e6c63b9
Alice Chan <alicebay10@yahoo.co.uk>
2024-10-01 12:11:13 +0000

    Adds secret key.

--- bubble/settings.py
+++ bubble/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^9pyibm-5ktw)^*t9^$mod%1$fa%)z8o&g=005w^2u03t%#e)y'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
