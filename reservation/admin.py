c2664b43fcad44e3505b16329c75687c97d89310
Alice Chan <alicebay10@yahoo.co.uk>
2024-10-01 12:47:22 +0000

    adds a model

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Reservation
 
 # Register your models here.
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    """
+    Allows admins a quick overview of all bookings,
+    with the ability to filter by date and time for a precise overview.
+    intended for use when seating walk-in customers on a given day or
+    making business decsions. Also allows for search by booking lead.
+
+    Containts methods to accept or decline the bookings within the dropdown.
+    """
+    list_display = ('lead', 'date', 'time', 'guests', 'status',)
+    list_filter = ('date', 'time',)
+    search_fields = ('lead',)
+    actions = ['accept_booking', 'decline_booking']
--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# A tuple to hold the status key for the Booking model.
+STATUS = ((0, "pending"), (1, "accepted"), (2, "declined"))
 
 # Create your models here.
+class Reservation(models.Model):
+    """
+    The model for the booking app.
+
+    Inherits the user from account sign up.
+    Stores the booking: lead, email, mobile, date, time
+    and how many guests for each individual booking.
+    Plus any special requirement notes.
+
+    Defaults the booking status to 'pending' using the above tuple.
+
+    Validation for the mobile field is handled with Django's inbuilt
+    RegexValidator.
+    """
+    # Foreign Key from the User model
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # User Info needed for Booking
+    lead = models.CharField(max_length=200, blank=False)
+    email = models.CharField(max_length=200, blank=False)
+    # Contact Number for Booking & Validator
+    
+    # Date of Booking
+    date = models.DateField(blank=False)
+    # Time of Booking
+    time = models.TimeField(blank=False)
+    # Special Requests for Booking
+    notes = models.TextField(max_length=200)
+    # Number of Guests on Booking
+    guests = models.PositiveIntegerField(blank=False)
+    # Booking Status - status updates handled in admin
+    status = models.IntegerField(choices=STATUS, default=0)
